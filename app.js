d58a4a003247d1c0957f3de0166b3ef8401f8daf
vaishnaviingole <vaishnavingole54@gmail.com>
2023-07-02 08:10:54 +0530

    Namaste React Inception 01

---
+++ app.js
@@ -0,0 +1,6 @@
+const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am An h1 tag"),React.createElement("h2",{},"I am An h1 tag")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I am An h1 tag"),React.createElement("h2",{},"I am An h1 tag")]),
+]);
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello World form React");
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+      root.render(parent);
