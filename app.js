9312c4cd4916a648ec86c3adc3b0d0d472898117
vaishnaviingole <vaishnavingole54@gmail.com>
2023-07-11 22:51:40 +0530

    day2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am An h1 tag"),React.createElement("h2",{},"I am An h1 tag")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I am An h1 tag"),React.createElement("h2",{},"I am An h1 tag")]),
 ]);
 
