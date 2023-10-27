773ecbe505186b4a4a64f747728471919e00b628
vaishnaviingole <vaishnavingole54@gmail.com>
2023-10-27 13:23:05 +0530

    Done with useState

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,24 +1,48 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
+import { useEffect } from "react";
+import resObj from "../utils/mockData";
 const Body = () => {
 
-  
-    return(
-      <div className="body">
-        <div classname="filter">
-           <button className="filter-btn" onClick={()=>{
-            console.log("btn working");}}>
-                Top Rated Restaurants
-                </button>
-            </div>
-        <div className="res-container">
-        {resObj.map((restaurant) => {
-        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-      })}
-          
-          
-        </div>
+  const [listOfRestaurants, setListOfRestaurant] = useState(resObj);
+  useEffect(() => { fetchData(); }, []);
+  const fetchData = () => {
+    const data = fetch()
+  };
+  return (
+    <div className="body">
+
+
+      <div class="search">
+        <form action="#">
+          <input type="search" placeholder="search Restaurant" name="search"></input>
+          <button>
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
       </div>
-    )
-  }
-  export default Body;
+
+
+      <div classname="filter">
+        <button className="filter-btn" onClick={() => {
+          console.log("btn working");
+          const filteredList = listOfRestaurants.filter((res) => res.data.avgRating > 4
+          );
+          setListOfRestaurant(filteredList);
+        }}>
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => {
+          return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
+        })}
+
+
+      </div>
+
+    </div>
+  )
+}
+export default Body;
